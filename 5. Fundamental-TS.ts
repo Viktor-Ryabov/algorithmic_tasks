678b226be4e4abd313f7418347f1b3cda92b400c
Viktor Ryabov <iprva@ya.ru>
2023-03-13 11:01:38 +0300

    added kata 5.3

--- 5. Fundamental-TS.ts
+++ 5. Fundamental-TS.ts
@@ -32,3 +32,20 @@ export function doubleChar(str: string): string {
     }
     return newArr.join("");
 }
+
+// *   *   *   *   *   *   
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str: string, ending: string): boolean {
+    if (ending == "" || str == ending){
+      return true;
+    }
+    return str.indexOf(ending) == 0? false : str.split(`${ending}`).at(-1) === "";
+  }
+
